1796d70c682625b1b4e4ae958c3af5d18ca9c016
Edeksender <edukas@luukku.com>
2018-06-11 14:39:07 +0200

    calc works?
    Now it's a proper calculator, in addition to giving the answers on if the code is correct

--- AR_APP - testi/Assets/Scripts/Vittu.cs
+++ AR_APP - testi/Assets/Scripts/Vittu.cs
@@ -12,43 +12,139 @@ public class Vittu : MonoBehaviour
     Text LogField;
 
     string inputString;
-    
+    int[] number = new int[2];
+    int[] koodi = new int[4];
+    int[] answer = new int[] { 4, 5, 7, 8 };
+    string operatorSymbol;
+    int i=0;
+    int f = 0;
+    int n = 0;
+    int result;
+    bool displayedResults = false;
+    bool NotClear = false;
+
+
 
     public void Pressed()
     {
+        if (displayedResults == true)
+        {
+            InputField.text = "";
+            inputString = "";
+            displayedResults = false;
+        }
+        if (NotClear == true)
+        {
+            LogField.text = "";
+            NotClear = false;
+        }
         Debug.Log(EventSystem.current.currentSelectedGameObject.name);
 
         string value = EventSystem.current.currentSelectedGameObject.name;
+        
+        
 
-        if (value == "Enter")
-        {
-            switch (inputString)
-            {
-                case "0000":
-                    Debug.Log(0000);
-                    inputString = "0000";
-                    LogField.text += inputString + "\n";
-                    break;
-                case "4568":
-                    inputString = "Remove page 1 and take page 2";
-                    LogField.text += inputString + "\n";
-                    break;
-                default:
-                    Debug.Log("lol nope");
-                    break;
-            }
-            inputString = "";
-            InputField.text = inputString;
-        }
+        int arg;
 
-        else
+
+        if (int.TryParse(value, out arg))
         {
             inputString += value;
+            koodi[i] = arg;
+            i++;
+        }
+        else
+        {
+            switch (value)
+            {
+                case "+":
+                    int.TryParse(inputString, out arg);
+                    number[0] = arg;
+                    operatorSymbol = value;
+                    displayedResults = true;
+                    break;
+                case "-":
+                    int.TryParse(inputString, out arg);
+                    number[0] = arg;
+                    operatorSymbol = value;
+                    displayedResults = true;
+                    break;
+                case "*":
+                    int.TryParse(inputString, out arg);
+                    number[0] = arg;
+                    operatorSymbol = value;
+                    displayedResults = true;
+                    break;
+                case "/":
+                    int.TryParse(inputString, out arg);
+                    number[0] = arg;
+                    operatorSymbol = value;
+                    displayedResults = true;
+                    break;
+                case "C":
+                    inputString = "";
+                    InputField.text = "";
+                    number = new int[2];
+                    koodi = new int[4];
+                    i = 0;
+                    break;
+                case "Enter":
+                    
+                    if (operatorSymbol != null)
+                    {
+                        switch (operatorSymbol)
+                        {
+                            case "+":
+                                int.TryParse(inputString, out arg);
+                                number[1] = arg;
+                                result = number[0] + number[1];
+                                break;
+                            case "-":
+                                int.TryParse(inputString, out arg);
+                                number[1] = arg;
+                                result = number[0] - number[1];
+                                break;
+                            case "*":
+                                int.TryParse(inputString, out arg);
+                                number[1] = arg;
+                                result = number[0] * number[1];
+                                break;
+                            case "/":
+                                int.TryParse(inputString, out arg);
+                                number[1] = arg;
+                                result = number[0] / number[1];
+                                break;
 
-            InputField.text = inputString;
+                        }
+                    }
+                    else
+                    {
+                        foreach (int digit in koodi)
+                        {
+                            if (koodi[f] == answer[f])
+                            {
+                                LogField.text += f + 1 + " correct\n";
+                            }
+                            else
+                            {
+                                LogField.text += f + 1 + " false\n";
+                            }
+                            f++;
+                        }
+                    }
+
+
+                    displayedResults = true;
+                    NotClear = true;
+                    inputString = result.ToString();
+                    number = new int[2];
+                    koodi = new int[4];
+                    i = n;
+                    f = n;
+                    break;
+            }
         }
 
+        InputField.text = inputString;
     }
-
-
 }
