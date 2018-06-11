5158e2a1b07f7e7c9286fcc542f1885c7875787b
Toonyoshi123 <lisasijnstra@live.nl>
2018-06-11 12:25:19 +0200

    added notifications

---
+++ AR_APP - testi/Assets/Notifications.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notifications : MonoBehaviour {
+
+	public int currentProgress = 0;
+	string[] hints = { };
+	string[] updates = { };
+
+	public void GiveHint()
+	{
+		//changing text and image of APP
+
+
+
+
+		//play animation
+	}
+
+	public void AppUpdate(string update)
+	{
+		// change text and image of App
+
+		//play animation
+	}
+}
