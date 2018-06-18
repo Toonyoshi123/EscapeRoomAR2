261010d8c1aca62eefcd0bbf3fc049af6b369cba
Toonyoshi123 <lisasijnstra@live.nl>
2018-06-18 15:31:09 +0200

    messages functionality finished
    Now we just need to content in there

---
+++ AR_APP - testi/Assets/APP_CONTENT/ChatRoom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ChatRoom : MonoBehaviour {
+
+	public GameObject chat;
+
+	float currentposition = 837f;
+	Text chatMessage;
+
+	//create a new line of text in the contents gameObject, set this as their parent and give it all a set size and location compared to each other.
+	public void NewLine(string line, int lines)
+	{
+		GameObject newChat = Instantiate (chat);
+		//create the text and it's image
+		chatMessage = newChat.GetComponentInChildren<Text>();
+			
+		chatMessage.GetComponent<Text>().text = line;
+		chatMessage.GetComponent<Text>().fontSize = 55;
+		chatMessage.GetComponent<RectTransform>().sizeDelta = new Vector2(585f, (lines * 60f));
+
+		//set the size and position(319.4f, (currentposition - (lines * 60f) - 10f),0f)
+		newChat.gameObject.GetComponent<RectTransform> ().transform.Translate(400f,(currentposition - (lines * 60f) - 10f),0f);
+		newChat.gameObject.GetComponent<RectTransform> ().sizeDelta = new Vector2 (595f,lines * 60f);
+
+		newChat.transform.SetParent (this.transform);
+		//update the position for the next message
+		currentposition -= (lines * 60f) + 30f;
+
+	}
+}
---
+++ AR_APP - testi/Assets/APP_CONTENT/SoundManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	[SerializeField]
+	AudioSource thisSource;
+
+	[SerializeField]
+	AudioSource otherSource1;
+	[SerializeField]
+	AudioSource otherSource2;
+	[SerializeField]
+	AudioSource otherSource3;
+
+	public Sprite pauseButton;
+	public Sprite playButton;
+
+	public string fileName;
+	public string fileNamePlaying;
+
+	public void CheckSituation()
+	{
+		if (thisSource.isPlaying) {
+			PauseSound ();
+		} else {
+			PlaySound ();
+		}
+	}
+
+	public void PlaySound()
+	{
+		if(!otherSource1.isPlaying && !otherSource2.isPlaying && !otherSource3.isPlaying)
+		{
+			thisSource.Play ();
+			//change image
+			this.GetComponent<Image>().sprite = pauseButton;
+			this.GetComponentInChildren<Text> ().text = fileNamePlaying;
+		}
+	}
+
+	public void PauseSound()
+	{
+		thisSource.Pause ();
+		//change image
+		this.GetComponent<Image>().sprite = playButton;
+		this.GetComponentInChildren<Text> ().text = fileName;
+	}
+
+	void Update()
+	{
+		if(!thisSource.isPlaying)
+		{
+			this.GetComponent<Image>().sprite = playButton;
+			this.GetComponentInChildren<Text> ().text = fileName;
+		}
+	}
+}
